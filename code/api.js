532048f80e6f483bf22e14170d0a946d6b4a0d1a
pulk66-s <hugo.marrasse@epitech.eu>
2023-01-20 23:50:58 +0100

    add of some call function

--- code/api.js
+++ code/api.js
@@ -9,3 +9,17 @@ function make_request(url, callback) {
     .then(response => response.json())
     .then(data => callback(data))
 }
+
+function random_cat_api(callback) {
+    make_request("https://api.thecatapi.com/v1/images/search", callback)
+}
+
+function random_dog_api(callback) {
+    make_request("https://dog.ceo/api/breeds/image/random", callback)
+}
+
+// latitute and longitude
+// latiture and longitude for Montpellier is 43.610769, 3.876716
+function get_current_weather(lat, lon, callback) {
+    make_request(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=811047b194123d4a5ab580a437b933aa`, callback)
+}
